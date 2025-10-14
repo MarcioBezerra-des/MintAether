6a37b1f740c973fe0fd2054039e72382df14efce
Marcio.Junior <marciobcavalcantijunior@gmail.com>
2025-10-13 22:48:58 -0400

    feat: Estrutura inicial do projeto com Spring Boot e Docker

---
+++ src/main/java/com/nftgenerator/MintAether/MintAetherApplication.java
@@ -0,0 +1,13 @@
+package com.nftgenerator.MintAether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintAetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintAetherApplication.class, args);
+	}
+
+}
