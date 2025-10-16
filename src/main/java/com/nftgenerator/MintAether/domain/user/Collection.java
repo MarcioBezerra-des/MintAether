5bdfc68a2decad9498c84dffc64d116f18ec6009
Marcio.Junior <marciobcavalcantijunior@gmail.com>
2025-10-16 07:02:19 -0400

    feat(domain): Add Collection entity and repository

--- src/main/java/com/nftgenerator/MintAether/domain/user/Collection.java
+++ src/main/java/com/nftgenerator/MintAether/domain/user/Collection.java
@@ -1,12 +1,21 @@
 package com.nftgenerator.MintAether.domain.user;
 
-import com.nftgenerator.MintAether.domain.user.User;
-import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.Instant;
-import java.util.UUID;
 
 @Entity(name = "collections")
 @Table(name = "collections")
@@ -21,11 +30,12 @@ public class Collection {
 
     @Column(nullable = false)
     private String name;
+
     private String description;
 
     @ManyToOne
     @JoinColumn(name = "creator_id", nullable = false)
-    private Instant creator;
+    private User creator; // <-- A CORREÇÃO ESTÁ AQUI
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
